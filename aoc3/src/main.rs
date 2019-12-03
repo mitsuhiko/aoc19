a0292050d148e024e9a83a971b8c8ba97e690b75
Armin Ronacher <armin.ronacher@active-4.com>
2019-12-03 16:51:23 +0100

    added part 2 of day 3

--- aoc3/src/main.rs
+++ aoc3/src/main.rs
@@ -1,39 +1,56 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeMap;
 
-fn find_intersection(input: &str) -> i32 {
-    let mut grid: BTreeMap<(i32, i32), BTreeSet<u8>> = Default::default();
+enum DistanceMethod {
+    Manhattan,
+    WireLength,
+}
+
+fn find_intersection(input: &str, method: DistanceMethod) -> i32 {
+    let mut grid: BTreeMap<(i32, i32), BTreeMap<u8, i32>> = Default::default();
     let mut wires = 0u8;
 
     for line in input.lines() {
-        let mut x = 0;
-        let mut y = 0;
+        let mut x = 0i32;
+        let mut y = 0i32;
         let wire = wires;
         wires += 1;
+        let mut distance = 0;
         for item in line.split(',') {
             let (step_x, step_y) = match item.chars().next().unwrap() {
                 'L' => (-1, 0),
                 'R' => (1, 0),
                 'D' => (0, -1),
                 'U' => (0, 1),
-                _ => panic!("bad input")
+                _ => panic!("bad input"),
             };
 
             for _ in 0..item[1..].parse::<i32>().unwrap() {
                 x += step_x;
                 y += step_y;
-                grid.entry((x, y)).or_default().insert(wire);
+                distance += step_x.abs() + step_y.abs();
+                *grid.entry((x, y)).or_default().entry(wire).or_default() += distance;
             }
         }
     }
 
-    grid
-        .into_iter()
+    grid.into_iter()
         .filter(|(_, matches)| matches.len() as u8 == wires)
-        .map(|((x, y), _)| x.abs() + y.abs())
+        .map(|((x, y), wires)| match method {
+            DistanceMethod::Manhattan => x.abs() + y.abs(),
+            DistanceMethod::WireLength => wires.values().sum::<i32>(),
+        })
         .min()
         .unwrap_or(0)
 }
 
 fn main() {
-    println!("part 1: {}", find_intersection(include_str!("../input.txt")));
+    let input = include_str!("../input.txt");
+    println!(
+        "part 1: {}",
+        find_intersection(input, DistanceMethod::Manhattan)
+    );
+    println!(
+        "part 2: {}",
+        find_intersection(input, DistanceMethod::WireLength)
+    );
 }
