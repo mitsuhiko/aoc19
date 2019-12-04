89468828bdc3b9dbf846c2297e1c2b4f648f5ca7
Armin Ronacher <armin.ronacher@active-4.com>
2019-12-04 22:31:04 +0100

    neater day 4

--- aoc4/src/main.rs
+++ aoc4/src/main.rs
@@ -1,48 +1,34 @@
-use smallvec::SmallVec;
+use std::cmp::Ordering;
 
 fn reverse_digit_iterator(mut num: u32) -> impl Iterator<Item = u8> {
     std::iter::from_fn(move || match num {
         0 => None,
-        _ => {
+        _ => Some({
             let digit = num % 10;
             num /= 10;
-            Some(digit as _)
-        }
+            digit as _
+        }),
     })
 }
 
 fn check_part1(num: u32) -> bool {
-    let mut last_digit = !0;
-    let mut found_duplicate = false;
-
-    for digit in reverse_digit_iterator(num) {
-        if digit > last_digit {
-            return false;
-        } else if digit == last_digit {
-            found_duplicate = true;
-        }
-        last_digit = digit;
-    }
-
-    found_duplicate
+    reverse_digit_iterator(num)
+        .try_fold((!0, false), |(last, good), cur| match cur.cmp(&last) {
+            Ordering::Equal => Some((cur, true)),
+            Ordering::Less => Some((cur, good)),
+            Ordering::Greater => None,
+        })
+        .map_or(false, |(_, good)| good)
 }
 
 fn check_part2(num: u32) -> bool {
-    let mut last_digit = !0;
-    let mut groups = SmallVec::<[u8; 6]>::new();
-
-    for digit in reverse_digit_iterator(num) {
-        if digit > last_digit {
-            return false;
-        } else if digit == last_digit {
-            *groups.last_mut().unwrap() += 1;
-        } else {
-            groups.push(0);
-        }
-        last_digit = digit;
-    }
-
-    groups.into_iter().any(|x| x == 1)
+    reverse_digit_iterator(num)
+        .try_fold((0, !0, false), |(n, last, ok), cur| match cur.cmp(&last) {
+            Ordering::Equal => Some((n + 1, cur, ok)),
+            Ordering::Less => Some((0, cur, ok || n == 1)),
+            Ordering::Greater => None,
+        })
+        .map_or(false, |(n, _, ok)| ok || n == 1)
 }
 
 fn complexity<F: Fn(u32) -> bool>(start: u32, end: u32, func: F) -> usize {
@@ -50,6 +36,6 @@ fn complexity<F: Fn(u32) -> bool>(start: u32, end: u32, func: F) -> usize {
 }
 
 fn main() {
-    println!("part 1: {}", complexity(178416, 676461, check_part1));
-    println!("part 2: {}", complexity(178416, 676461, check_part2));
+    println!("part 1: {}", complexity(178_416, 676_461, check_part1));
+    println!("part 2: {}", complexity(178_416, 676_461, check_part2));
 }
