7a1fab5a9393b12f56915ae95933e8eda140bf47
Armin Ronacher <armin.ronacher@active-4.com>
2019-12-06 21:35:21 +0100

    added day 6

---
+++ aoc6/src/main.rs
@@ -0,0 +1,42 @@
+use std::collections::BTreeMap;
+
+type Graph<'a> = BTreeMap<&'a str, &'a str>;
+
+fn parse_graph(s: &str) -> Graph<'_> {
+    s.trim()
+        .lines()
+        .filter_map(|line| Some((line.find(')')?, line)))
+        .map(|(idx, line)| (&line[idx + 1..], &line[..idx]))
+        .collect()
+}
+
+fn walk<'graph, 'data>(
+    graph: &'graph Graph<'data>,
+    to: &'data str,
+) -> impl Iterator<Item = &'data str> + 'graph {
+    std::iter::successors(Some(to), move |&to| graph.get(to).copied()).skip(1)
+}
+
+fn count_orbits(graph: &Graph) -> usize {
+    graph.keys().fold(0, |x, to| x + walk(graph, to).count())
+}
+
+fn find_shortest_path(graph: &Graph, a: &str, b: &str) -> Option<usize> {
+    let b_orbits = walk(graph, b)
+        .enumerate()
+        .map(|(d, p)| (p, d))
+        .collect::<BTreeMap<_, _>>();
+    walk(graph, a)
+        .enumerate()
+        .filter_map(|(distance, point)| Some(distance + b_orbits.get(point)?))
+        .next()
+}
+
+fn main() {
+    let graph = parse_graph(include_str!("../input.txt"));
+    println!("part 1: {}", count_orbits(&graph));
+    println!(
+        "part 2: {}",
+        find_shortest_path(&graph, "YOU", "SAN").unwrap()
+    );
+}
