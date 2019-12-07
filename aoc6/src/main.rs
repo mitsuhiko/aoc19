788fd2fbaecbb1f9d77959201aeeb503b2d35099
Armin Ronacher <armin.ronacher@active-4.com>
2019-12-07 02:00:58 +0100

    prefer list to btreemap for measure_path

--- aoc6/src/main.rs
+++ aoc6/src/main.rs
@@ -4,8 +4,7 @@ use std::iter::successors;
 type Graph<'a> = BTreeMap<&'a str, &'a str>;
 
 fn parse_graph(s: &str) -> Graph<'_> {
-    s.trim()
-        .lines()
+    s.lines()
         .filter_map(|line| Some((line.find(')')?, line)))
         .map(|(idx, line)| (&line[idx + 1..], &line[..idx]))
         .collect()
@@ -20,13 +19,10 @@ fn count_orbits(graph: &Graph) -> usize {
 }
 
 fn measure_path(graph: &Graph, a: &str, b: &str) -> Option<usize> {
-    let b_orbits = walk(graph, b)
-        .enumerate()
-        .map(|(distance, point)| (point, distance))
-        .collect::<BTreeMap<_, _>>();
+    let b_orbits: Vec<_> = walk(graph, b).collect();
     walk(graph, a)
         .enumerate()
-        .filter_map(|(distance, point)| Some(distance + b_orbits.get(point)?))
+        .filter_map(|(d, p)| Some(d + b_orbits.iter().rposition(|&x| x == p)?))
         .next()
 }
 
